54bd6f166ba01a067f2057d2faca0f4b98b1975d
Erik Grove and Sasa Schwartz <erik+sasa@>
2020-01-22 11:08:36 -0800

    add js functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,35 @@ $(document).ready(function(){
   $("#fortune-survey").submit(function(event){
     event.preventDefault();
 
+    var scoreCount = 0;
+    var checkedCount = 0;
 
-  })
+    $("input:checkbox[name=tea]:checked").each(function(){
+      checkedCount++;
+      if ($(this).hasClass("boring")) {
+        scoreCount += 1;
+      }
+      if ($(this).hasClass("creative")) {
+        scoreCount += 2;
+      }
+      if ($(this).hasClass("rebellious")) {
+        scoreCount += 3;
+      }
+    })
+
+    var finalScore = Math.round(scoreCount/checkedCount);
+
+    if (finalScore === 1){
+      $("#boring-result").show();
+    } else if (finalScore === 2){
+      $("#creative-result").show();
+    } else if (finalScore === 3){
+      $("#rebellious-result").show();
+    } else {
+      $("#no-answer").show();
+    }
+
+    $("#fortune-result").show();
+    $("#fortune-survey").hide();
+  });
 });
-
-  // $("form#transportation_survey").submit(function(event){
-  //   event.preventDefault();
-  //   $("#work-responses").show();
-  //   $("input:checkbox[name=work-transportation]:checked").each(function(){
-  //     var workTransportationMode = $(this).val();
-  //     $('#work-responses').append(workTransportationMode + "<br>");
-  //   });
-  //   $("#fun-responses").show();
-  //   $("input:checkbox[name=fun-transportation]:checked").each(function(){
-  //     var funTransportationMode = $(this).val();
-  //     $('#fun-responses').append(funTransportationMode + "<br>");
-  //   });
-  //   $('#transportation_survey').hide();
-  // });
